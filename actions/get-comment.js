992b634424dad4a5404f5be24c1f1b4842ce4db3
Hayeon <gkdus2001@gmail.com>
2023-08-17 17:04:55 +0900

    추천 ootd 시트 개발완료

--- actions/get-comment.js
+++ actions/get-comment.js
@@ -1,8 +1,15 @@
 import axios from 'axios'
 
-export const getPop = async () => {
+export const getComment = async () => {
+  const url = `${process.env.NEXT_PUBLIC_APP_URL}/api/comment`
+  const res = await axios.get(url)
+  console.log('+ res.data comment: ', res.data)
+  return res.data
+}
+
+export const getCommentToday = async () => {
   const url = `${process.env.NEXT_PUBLIC_APP_URL}/api/comment/today`
   const res = await axios.get(url)
-  console.log('+ res.data : ', res.data)
+  console.log('+ res.data comment today: ', res.data)
   return res.data
 }
